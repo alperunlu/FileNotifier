b81de7dac8e31ddc5cc2245c23bd7755cd9c5362
Alper Ünlü <alperunlu@ieee.org>
2021-06-10 23:21:48 +0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace FileNotifierV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.popupNotifier1 = new Tulpep.NotificationWindow.PopupNotifier();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(4, 106);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(565, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
+            this.fileSystemWatcher1.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            this.fileSystemWatcher1.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher1_Renamed);
+            // 
+            // popupNotifier1
+            // 
+            this.popupNotifier1.ContentFont = new System.Drawing.Font("Tahoma", 8F);
+            this.popupNotifier1.ContentText = null;
+            this.popupNotifier1.Image = null;
+            this.popupNotifier1.IsRightToLeft = false;
+            this.popupNotifier1.OptionsMenu = null;
+            this.popupNotifier1.Size = new System.Drawing.Size(400, 100);
+            this.popupNotifier1.TitleFont = new System.Drawing.Font("Segoe UI", 9F);
+            this.popupNotifier1.TitleText = null;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(4, 72);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(82, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "File Created";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(111, 72);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(82, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "File Deleted";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(218, 72);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(91, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "File Renamed";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(334, 72);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(88, 17);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "File Changed";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(447, 72);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(114, 17);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "Include Subfolders";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Change Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Path:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(53, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(377, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(435, 204);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(134, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "www.github.com/alperunlu";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(373, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "C# implementation of a combination of file system watcher and pop-up notifier.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 223);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "FileNotifier";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private Tulpep.NotificationWindow.PopupNotifier popupNotifier1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Reflection;
+using Tulpep.NotificationWindow;
+
+namespace FileNotifierV2
+{
+    public partial class Form1 : Form
+    {
+        private string exePath = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void LogWrite(string logMessage)
+        {
+            exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            try
+            {
+                using (StreamWriter logFile = File.AppendText(exePath + "\\" + "log.txt"))
+                {
+                    logFile.Write("\r\n{0}", logMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }   
+
+
+        private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                string time = DateTime.Now.ToString("h:mm:ss");
+                string alert = string.Format("File created {0} {1} - {2}", e.FullPath, e.Name, time);
+                //MessageBox.Show(alert);
+                listBox1.BeginUpdate();
+                listBox1.Items.Add(alert);
+                listBox1.EndUpdate();
+                LogWrite(alert);
+                popupNotifier1.TitleText = "File Created!";
+                popupNotifier1.ContentText = alert;
+                popupNotifier1.Popup();
+            }
+        }
+
+        private void fileSystemWatcher1_Deleted(object sender, FileSystemEventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                string time = DateTime.Now.ToString("h:mm:ss");
+                string alert = string.Format("File deleted {0} {1} - {2}", e.FullPath, e.Name, time);
+                //MessageBox.Show(alert);
+                listBox1.BeginUpdate();
+                listBox1.Items.Add(alert);
+                listBox1.EndUpdate();
+                LogWrite(alert);
+                popupNotifier1.TitleText = "File Deleted!";
+                popupNotifier1.ContentText = alert;
+                popupNotifier1.Popup();
+            }
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, FileSystemEventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                string time = DateTime.Now.ToString("h:mm:ss");
+                string alert = string.Format("File renamed {0} {1} - {2}", e.FullPath, e.Name, time);
+                //MessageBox.Show(alert);
+                listBox1.BeginUpdate();
+                listBox1.Items.Add(alert);
+                listBox1.EndUpdate();
+                LogWrite(alert);
+                popupNotifier1.TitleText = "File Renamed!";
+                popupNotifier1.ContentText = alert;
+                popupNotifier1.Popup();
+            }
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (checkBox4.Checked == true)
+            {
+                string time = DateTime.Now.ToString("h:mm:ss");
+                string alert = string.Format("File changed {0} {1} - {2}", e.FullPath, e.Name, time);
+                //MessageBox.Show(alert);
+                listBox1.BeginUpdate();
+                listBox1.Items.Add(alert);
+                listBox1.EndUpdate();
+                LogWrite(alert);
+                popupNotifier1.TitleText = "File Changed!";
+                popupNotifier1.ContentText = alert;
+                popupNotifier1.Popup();
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            PathSelect();
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox5.Checked == true)
+            {
+                fileSystemWatcher1.IncludeSubdirectories = true;
+            }
+            else
+            {
+                fileSystemWatcher1.IncludeSubdirectories = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PathSelect();
+        }
+
+        private void PathSelect()
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    //MessageBox.Show(fbd.SelectedPath.ToString());
+                    textBox1.Text = fbd.SelectedPath;
+                    fileSystemWatcher1.Path = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.github.com/alperunlu");
+        }
+    }
+}
